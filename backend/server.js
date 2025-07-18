527f68326151fbb2d4b6358dd0c373f8db146627
Yogesh <172988161+YOGESH-OYS@users.noreply.github.com>
2025-07-18 13:53:07 +0530

    DEPLOYMENT - 01

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,13 @@ app.use(passport.initialize());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/user", require("./routes/user"));
 
+const PORT = process.env.PORT || 5000 ;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(5000, () =>
-      console.log("Server running on http://localhost:5000")
+    app.listen(PORT, () =>
+      console.log(`Server running on ${PORT}`)
     );
   })
   .catch((err) => console.error(err));
